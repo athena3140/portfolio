58868bb2cb0e9da5aa0d3748cb73f850cdf7c241
athena <athena3140@gmail.com>
2023-06-12 13:11:52 +0630

    Add/edit files

--- static/script.js
+++ static/script.js
@@ -2,215 +2,132 @@ let audio = new Audio("./static/audio.mp3");
 icon = document.getElementById("upIcon");
 easterImg = document.getElementById("imgclick");
 loader = document.getElementById("loader");
-document.querySelectorAll("body *").forEach((e) => {
-	e.tabIndex = -1;
+document.querySelectorAll("body *").forEach((a) => {
+	a.tabIndex = -1;
 });
-//Icon That Go To Top
 window.addEventListener("scroll", () => {
-	//Show To Top Icon When It's Under
-	if (scrollY > 500) {
-		icon.style.cssText = `opacity: 1;pointerEvents: "auto"`;
-	} else {
-		icon.style.cssText = `opacity: 0;pointerEvents: "none";bottom: 0%;transform:scale(0);border-radius:0`;
-	}
-
-	//Icon Onclick That Scroll To Top
-	icon.addEventListener("click", function (e) {
-		e.preventDefault();
+	500 < scrollY
+		? (icon.style.cssText = 'opacity: 1;pointerEvents: "auto"')
+		: (icon.style.cssText = 'opacity: 0;pointerEvents: "none";bottom: 0%;transform:scale(0);border-radius:0');
+	icon.addEventListener("click", function (a) {
+		a.preventDefault();
 		$("#upIcon").tooltip("hide");
 		window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
 	});
 });
-
-//Title Change Function When Page Is Hidden
 document.addEventListener("visibilitychange", () => {
-	if (document.hidden) {
-		document.title = `| \u00A0\u00A0See You Later !`;
-	} else {
-		document.title = `| \u00A0\u00A0Welcome !`;
-		setTimeout(() => {
-			document.title = `| \u00A0\u00A0Athena's Portfolio`;
-		}, 2000);
-	}
+	document.hidden
+		? (document.title = "| \u00a0\u00a0 See You Later !")
+		: ((document.title = "| \u00a0\u00a0 Welcome !"),
+		  setTimeout(() => {
+				document.title = "| \u00a0\u00a0Athena's Portfolio";
+		  }, 2e3));
 });
-
-if (window.location.href.indexOf("#formSubmit") > -1) {
-	$("#loader").remove();
-	$("form").css("opacity", "1");
-	$("form").css("pointer-events", "none");
-	$(".formAlert").hide();
-	setTimeout(() => {
-		$(".formAlert").show();
-		$("form").css("opacity", "0");
-		setTimeout(() => {
-			$("form").css("opacity", "1");
-			$("form").css("pointer-events", "auto");
-			$(".formAlert").hide();
-		}, 9000);
-	}, 1000);
-} else {
-	$(".formAlert").hide();
-}
-//Load Function
 window.addEventListener("load", () => {
-	if (window.location.href.indexOf("#formSubmit") > -1) {
-		window.scrollTo({
-			top: document.getElementById("contact").offsetTop,
-			behavior: "instant",
-		});
+	document.body.style.cssText = "overflow:hidden; height: 100svh";
+	icon.style.cssText = "opacity: 0; pointer-events: none";
+	setTimeout(() => {
+		loader.style.cssText = "opacity:0;pointer-events:none";
+		500 > scrollY
+			? (document.getElementById("spinner").style.cssText = "transform:scale(7.5)")
+			: (document.getElementById("spinner").style.cssText = "transform:scale(.2)");
 		setTimeout(() => {
-			$(".check-icon").show();
-		}, 500);
-	} else {
-		document.body.style.cssText = "overflow:hidden; height: 100svh";
-		icon.style.cssText = "opacity: 0; pointer-events: none";
-		//Hiding The Loader After 0.3s of page Load
-		setTimeout(() => {
-			loader.style.cssText = "opacity:0;pointer-events:none";
-			if (scrollY < 500) {
-				document.getElementById("spinner").style.cssText = `transform:scale(7.5)`;
-			} else {
-				document.getElementById("spinner").style.cssText = `transform:scale(.2)`;
-			}
-			setTimeout(() => {
-				document.getElementById("home").classList.add("animate__animated", "animate__zoomIn");
-				if (scrollY < 500) {
-					document.getElementById("navbar").classList.add("animate__animated", "animate__zoomIn");
-				}
-			}, 20);
-			document.body.style.cssText = "overflow: auto; height: auto";
-		}, 500);
-	}
+			document.getElementById("home").classList.add("animate__animated", "animate__zoomIn");
+			500 > scrollY &&
+				document.getElementById("navbar").classList.add("animate__animated", "animate__zoomIn");
+		}, 20);
+		document.body.style.cssText = "overflow: auto; height: auto";
+	}, 500);
+	setTimeout(() => {
+		$("#loader").hide();
+	}, 1e3);
 });
-
-//Add More Height To Scroll Element So that Id hash link will not go under nav bar
-document.querySelectorAll(".nav-item a").forEach((link) => {
-	link.addEventListener("click", function (event) {
-		event.preventDefault();
-		var linkHref = event.target.getAttribute("href");
-		var element = document.querySelector(linkHref);
-		location.hash = linkHref;
+document.querySelectorAll(".nav-item a,.list-inline-item a").forEach((a) => {
+	a.addEventListener("click", function (b) {
+		b.preventDefault();
+		b = b.target.getAttribute("href");
+		var f = document.querySelector(b);
+		location.hash = b;
 		setTimeout(function () {
 			history.replaceState({}, document.title, window.location.pathname);
-		}, 1000);
-		var elementY = element.getBoundingClientRect().top + window.pageYOffset;
-		window.scrollTo({
-			top: elementY - document.querySelector(".navbar").offsetHeight - 10,
-			behavior: "smooth",
-		});
+		}, 1e3);
+		b = f.getBoundingClientRect().top + window.pageYOffset;
+		window.scrollTo({ top: b - document.querySelector(".navbar").offsetHeight - 10, behavior: "smooth" });
 	});
 });
-
-// setTimeout(() => {
-// 	console.clear();
-// }, 4000);
-
-let clickCount = 0;
-let fadeOutInterval;
-easterImg.addEventListener("click", () => {
-	clickCount += 1;
-	if (clickCount == 5) {
-		toolTip[0].disable();
-		clickCount = 0;
-		audio.volume = 0.5;
-		audio.play();
-		easterAlert[0].show();
-	}
-	easterImg.classList.remove("shadow-sm");
-	easterImg.style.transform = "scale(.97)";
-	easterImg.style.boxShadow = `0px 0px 90px -10px rgba(190,190,190,1)`;
-	easterImg.style.border = `1px solid #c22424`;
-	setTimeout(() => {
-		easterImg.style.transform = "scale(1)";
-		easterImg.style.boxShadow = ``;
-		easterImg.style.border = `none`;
-	}, 130);
-});
-
-easterImg.addEventListener("dblclick", () => {
-	easterAlert[0].hide();
-	toolTip[0].enable();
-	let volume = audio.volume;
-	if (!audio.paused) {
-		const fadeOutInterval = setInterval(() => {
-			if (volume >= 0) {
-				volume -= 0.05;
-				audio.volume = volume;
-			} else {
-				clearInterval(fadeOutInterval);
-				audio.pause();
-			}
-		}, 100);
-	}
-});
-
+let clickCount = 0,
+	fadeOutInterval;
 function mousedown() {
 	easterImg.classList.remove("shadow-sm");
 	easterImg.style.transform = "scale(.97)";
-	easterImg.style.boxShadow = `0px 0px 90px -10px rgba(190,190,190,1)`;
-	easterImg.style.border = `1px solid #c22424`;
+	easterImg.style.boxShadow = "0px 0px 90px -10px rgba(190,190,190,1)";
+	easterImg.style.border = "1px solid #c22424";
 }
 function mouseup() {
 	easterImg.classList.add("shadow-sm");
 	easterImg.style.transform = "scale(1)";
-	easterImg.style.boxShadow = ``;
-	easterImg.style.border = `none`;
+	easterImg.style.boxShadow = "";
+	easterImg.style.border = "none";
 }
-//
-//
-//
-//
-//
-//
-//
-//
-//
-
-const navLinks = document.querySelectorAll(".navbar-nav a");
-const sections = document.querySelectorAll("section");
-
-const options = {
-	threshold: 0.5, // When 50% of the section is visible
-};
-
-const observer = new IntersectionObserver((entries) => {
-	entries.forEach((entry) => {
-		if (entry.isIntersecting) {
-			const sectionId = entry.target.getAttribute("id");
-			setActiveClass(sectionId);
-		} else {
-			const sectionId = entry.target.getAttribute("id");
-			setInactiveClass(sectionId);
+if (
+	(easterImg.addEventListener("click", () => {
+		5 == (clickCount += 1) &&
+			(toolTip[0].disable(), (clickCount = 0), (audio.volume = 0.5), audio.play(), easterAlert[0].show());
+		easterImg.classList.remove("shadow-sm");
+		easterImg.style.transform = "scale(.97)";
+		easterImg.style.boxShadow = "0px 0px 90px -10px rgba(190,190,190,1)";
+		easterImg.style.border = "1px solid #c22424";
+		setTimeout(() => {
+			easterImg.style.transform = "scale(1)";
+			easterImg.style.boxShadow = "";
+			easterImg.style.border = "none";
+		}, 130);
+	}),
+	easterImg.addEventListener("dblclick", () => {
+		easterAlert[0].hide();
+		toolTip[0].enable();
+		let a = audio.volume;
+		if (!audio.paused) {
+			let b = setInterval(() => {
+				0 <= a ? ((a -= 0.05), (audio.volume = a)) : (clearInterval(b), audio.pause());
+			}, 100);
 		}
-	});
-}, options);
-
-sections.forEach((section) => {
-	observer.observe(section);
-});
-
-function setActiveClass(id) {
-	navLinks.forEach((navLink) => {
-		navLink.parentElement.classList.remove("outtro");
-		if (navLink.getAttribute("href") === `#${id}`) {
-			navLink.parentElement.classList.add("navActive");
-		} else {
-			navLink.parentElement.classList.remove("navActive");
-		}
+	}),
+	992 > screen.width)
+)
+	console.log("under992");
+else {
+	let a = document.querySelectorAll(".navbar-nav a"),
+		b = document.querySelectorAll("section"),
+		f = new IntersectionObserver(
+			(c) => {
+				c.forEach((d) => {
+					d.isIntersecting ? g(d.target.getAttribute("id")) : e(d.target.getAttribute("id"));
+				});
+			},
+			{ threshold: 0.5 }
+		);
+	function g(c) {
+		a.forEach((d) => {
+			d.parentElement.classList.remove("outtro");
+			d.getAttribute("href") === `#${c}`
+				? d.parentElement.classList.add("navActive")
+				: d.parentElement.classList.remove("navActive");
+		});
+	}
+	function e(c) {
+		a.forEach((d) => {
+			d.getAttribute("href") === `#${c}` &&
+				(d.parentElement.classList.add("outtro"),
+				setTimeout(() => {
+					d.parentElement.classList.remove("navActive", "outtro");
+				}, 200));
+		});
+	}
+	b.forEach((c) => {
+		f.observe(c);
 	});
 }
-
-function setInactiveClass(id) {
-	navLinks.forEach((navLink) => {
-		if (navLink.getAttribute("href") === `#${id}`) {
-			navLink.parentElement.classList.add("outtro");
-			setTimeout(() => {
-				navLink.parentElement.classList.remove("navActive", "outtro");
-			}, 200); // Remove the navActive and outtro classes after the animation is complete
-		}
-	});
-}
-
 $("#igHover").hover(
 	function () {
 		$("#hoveredItem").text("/athena.3140");
@@ -219,3 +136,79 @@ $("#igHover").hover(
 		$("#hoveredItem").text("/athena3140");
 	}
 );
+const toolTip = tippy("#imgclick", {
+	content: "Click 5 Times To Activate The Easter Egg",
+	delay: [400, 200],
+	animation: "shift-toward",
+	theme: "myTheme",
+	multiple: !0,
+});
+/(Mobi|Android|iPhone|iPad|iPod)/i.test(navigator.userAgent) ||
+	tippy("#upIcon", { animation: "shift-toward", content: "Back To Top", placement: "left", hideOnClick: !0 });
+const easterAlert = tippy("#imgclick", {
+	content: "Double click to pause the song",
+	animation: "shift-toward",
+	placement: "left",
+	multiple: !0,
+	trigger: "manual",
+	hideOnClick: !1,
+	delay: [200, 200],
+});
+$(document).ready(function () {
+	$("input, textarea").each(function () {
+		$(this).data("placeholder", $(this).attr("placeholder"));
+		$(this).removeAttr("placeholder");
+	});
+	$("input, textarea").focus(function () {
+		"" === $(this).val() && $(this).attr("placeholder", $(this).data("placeholder"));
+	});
+	$("input, textarea").blur(function () {
+		"" === $(this).val() && $(this).removeAttr("placeholder");
+	});
+	$("#FORMDIV").css("min-height", $("#FORMDIV").height());
+	$("#submit").removeClass("disabled");
+	$("form").submit(function (a) {
+		a.preventDefault();
+		formData = $(this).serialize();
+		a = $("#name").val().trim();
+		var b = $("#email").val().trim(),
+			f = $("#subject").val().trim(),
+			g = $("#message").val().trim();
+		let e = "";
+		"" === a || "" === b || "" === f || "" === g
+			? ("" === a && (e += "<li>Name is required</li>"),
+			  "" === b && (e += "<li>Email is required</li>"),
+			  "" === f && (e += "<li>Subject is required</li>"),
+			  "" === g && (e += "<li>Message is required</li>"),
+			  $(".formError ul").html(e),
+			  $(".formError").slideDown())
+			: /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(b)
+			? ($(".formError").hide(),
+			  $("#submit").addClass("disabled"),
+			  $.ajax({
+					url: "https://formsubmit.co/ajax/athena3140@gmail.com",
+					method: "POST",
+					accepts: "application/json",
+					data: formData,
+					success: function (c) {
+						"true" == (c = JSON.parse(c)).success
+							? ($("form").hide(),
+							  $(".formAlert").show(),
+							  setTimeout(() => {
+									$("form").show();
+									$(".formAlert").hide();
+							  }, 9e3),
+							  $("#submit").removeClass("disabled"),
+							  $("input, textarea").val(""),
+							  $("input, textarea").blur())
+							: console.error("Form submission failed: " + c);
+					},
+					error: function (c) {
+						console.error(c);
+					},
+			  }))
+			: ((e += "Please enter a valid email address."),
+			  $(".formError").slideDown(),
+			  $(".formError ul").html(e));
+	});
+});
